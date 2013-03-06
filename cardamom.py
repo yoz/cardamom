d6b22c8e12010c79e95ded5bd2d703e6e6e7cff8
Yoyo Zhou <yozhov@gmail.com>
2013-03-06 09:29:21 -0800

    Initial version

---
+++ cardamom.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import json  # requires >2.6
+import os
+
+# PLAN:
+# Dump dances to dances.json. script src it directly.
+# Use templating to render it.
+# 
+
+class Dance(object):
+    #self.name = None
+    #self.author = None
+    #self.starting_formation = None
+    #self.tags = None
+    #self.body = None
+    # maybe later
+    #self.moves = None
+    #self.notes = None
+
+    def __init__(self, plaintext):
+        lines = plaintext.split('\n')
+        self.lines = lines
+        self.name = lines[0]
+        self.author = lines[1]
+        self.starting_formation = lines[2]
+        self.tags = []
+        for i in xrange(3, len(lines)):
+            # Each line is a tag until a blank line.
+            # Then everything is in the body.
+
+            # TODO: Body consists of dance figures, continuations, or blank lines.
+            # Figures are [phrase]? [timing] [figure]
+            # Continuations are non-blank, follow figures, and are indented
+
+            # After the body comes notes.
+            # Notes might contain variations, more instructions, etc.
+
+            # TODO: Can our template system deal with individual lines in notes
+            # possibly being figures?
+            # Handlebars might be able to deal.
+
+            if lines[i] == '':
+                self.body = lines[i:]
+                break
+            else:
+                self.tags.append(lines[i])
+
+    def AsDict(self):
+        return {'name': self.name,
+                'author': self.author,
+                'starting_formation': self.starting_formation,
+                'tags': self.tags,
+                'body': self.body}
+
+def ImportDir(src):
+    dances = []
+    for filename in os.listdir(src):
+        dances.append(Dance(open(os.path.join(src, filename)).read()))
+    return dances
+
+def DumpDances(dances):
+    return json.dumps(dict((x.name, x.lines) for x in dances))
---
+++ cardreader.js
@@ -0,0 +1,107 @@
+function requestURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  try {
+    xhr.onreadystatechange = function(state) {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          var text = xhr.responseText;
+          callback(text);
+        }
+      }
+    }
+    xhr.onerror = function(error) {
+      console.log("xhr error: " + JSON.stringify(error));
+      console.dir(error);
+    }
+    xhr.open("GET", url, true);
+    xhr.send({});
+  } catch(e) {
+    console.log("exception: " + e);
+  }
+}
+
+var barearms = 'barearms';
+var barearm = 'barearm';
+var armbands = 'bands';
+var armband = 'band';
+
+// All of these only match complete words.
+// TODO(yoz): capitalized versions!
+var genderFreeMap = {
+  'ladies': barearms,
+  'lady': barearm,
+  'gents': armbands,
+  'gent': armband,
+  'women': barearms,
+  'woman': barearm,
+  'men': armbands,
+  'man': armband,
+};
+
+// TODO(yoz): not in global context
+var gendered = true;
+var dances;
+var templateMain;
+// TODO(yoz): show multiple cards, rearrange them, index search, etc.
+var selectedDance;
+
+function redisplayMain() {
+  var elMain = document.getElementById('main');
+  var context = {lings: dances[selectedDance]};
+  var main = templateMain(context);
+  if (!gendered) {
+    for (var fromWord in genderFreeMap) {
+      if (genderFreeMap.hasOwnProperty(fromWord)) {
+        // TODO(yoz): can this be more efficient than making copies?
+        // TODO(yoz): don't rebuild regexps so much
+        var re = new RegExp('\\b' + fromWord + '\\b', 'g');
+        main = main.replace(re, genderFreeMap[fromWord]);
+      }
+    }
+  }
+  elMain.innerHTML = main;
+};
+
+function getSelectDance(name) {
+  return function() {
+    selectedDance = name;
+    redisplayMain();
+  };
+}
+
+function toggleGender(name) {
+  gendered = document.getElementById('gender').checked;
+  console.log(gendered);
+  redisplayMain();
+}
+
+window.onload = function() {
+  document.getElementById('gender').addEventListener('click', toggleGender);
+
+  requestURL('template_main.html', function(mainBlob) {
+    requestURL('template_index.html', function(indexBlob) {
+      var templateIndex = Handlebars.compile(indexBlob);
+      templateMain = Handlebars.compile(mainBlob);
+      requestURL('data/dances.json', function(danceBlob) {
+        // Populate index.
+        dances = JSON.parse(danceBlob);
+        var indexContext = {names: []};
+        for (var name in dances) {
+          if (dances.hasOwnProperty(name)) {
+            indexContext.names.push(name);
+          }
+        }
+        indexContext.names.sort();
+        var elIndex = document.getElementById('index');
+        elIndex.innerHTML = templateIndex(indexContext);
+        // Add click handlers to index.
+        var indexLinks = elIndex.getElementsByClassName('indexlink');
+        for (var i = 0; i < indexLinks.length; ++i) {
+          var alink = indexLinks[i].firstElementChild;
+          alink.addEventListener('click',
+                                 getSelectDance(alink.text));
+        }
+      });
+    });
+  });
+};
